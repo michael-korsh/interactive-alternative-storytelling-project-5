d45f9df382c6387fa7e8205507daef7c7e23eb00
michael-korsh <66495950+michael-korsh@users.noreply.github.com>
2021-11-18 09:43:08 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let tl1 = new gsap.timeline({paused:true});
+
+tl1.to("#correct-1", {duration: 1, backgroundColor:"#90ee90", border:"solid 2px #00A465"})
+  .to("#incorrect-1", {duration: 1, backgroundColor:"#FF7F7F", border:
+"solid 2px #D3202F"})
+  .to("#explanation-1", {duration: 1, display:"block"});
+
+let tl2 = new gsap.timeline({paused:true});
+
+tl2.to("#correct-2", {duration: 1, backgroundColor:"#90ee90", border:"solid 2px #00A465"})
+  .to("#incorrect-2", {duration: 1, backgroundColor:"#FF7F7F", border:
+"solid 2px #D3202F"})
+  .to("#explanation-2", {duration: 1, display:"block"});
+
+let tl3 = new gsap.timeline({paused:true});
+
+tl3.to("#correct-3", {duration: 1, backgroundColor:"#90ee90", border:"solid 2px #00A465"})
+  .to("#incorrect-3", {duration: 1, backgroundColor:"#FF7F7F", border:
+"solid 2px #D3202F"})
+  .to("#explanation-3", {duration: 1, display:"block"});
+
+let tl4 = new gsap.timeline({paused:true});
+
+tl4.to("#correct-4", {duration: 1, backgroundColor:"#90ee90", border:"solid 2px #00A465"})
+  .to("#incorrect-4", {duration: 1, backgroundColor:"#FF7F7F", border:
+"solid 2px #D3202F"})
+  .to("#explanation-4", {duration: 1, display:"block"});
+
+let tl5 = new gsap.timeline({paused:true});
+
+tl5.to("#correct-5", {duration: 1, backgroundColor:"#90ee90", border:"solid 2px #00A465"})
+  .to("#incorrect-5", {duration: 1, backgroundColor:"#FF7F7F", border:
+"solid 2px #D3202F"})
+  .to("#explanation-5", {duration: 1, display:"block"});
